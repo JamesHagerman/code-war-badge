7660808800618fda9bff65d93eb131d386551130
JamesHagerman <james.hagerman@gmail.com>
2018-12-12 23:01:45 -0800

    Using D7 as a status led to show that the GW is indeed publishing. Starting to try to organize some of this so that we can have this code run on the multiple hardware setups I have. Quite a mess. GW is set to use external mesh antenna.

--- src/code-war-badge.cpp
+++ src/code-war-badge.cpp
@@ -8,7 +8,7 @@
 #include "adafruit-led-backpack.h"
 #include "neopixel.h"
 
-SYSTEM_THREAD(ENABLED);
+//SYSTEM_THREAD(ENABLED);
 
 SerialLogHandler logHandler(LOG_LEVEL_TRACE);
 
@@ -21,8 +21,48 @@ Adafruit_7segment matrix = Adafruit_7segment();
 
 Adafruit_NeoPixel strip(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
 
+void selectExternalMeshAntenna() {
+#if (PLATFORM_ID == PLATFORM_ARGON)
+  digitalWrite(ANTSW1, 1);
+  digitalWrite(ANTSW2, 0);
+#elif (PLATFORM_ID == PLATFORM_BORON)
+  digitalWrite(ANTSW1, 0);
+#else
+  digitalWrite(ANTSW1, 0);
+  digitalWrite(ANTSW2, 1);
+#endif
+}
+
+
+//void pingMesh() {
+//  Mesh.publish("mesh-msg", "GW ping");
+//}
+//Timer meshPinger(100, pingMesh); // GWs should ping mesh ever 100ms
+bool meshStatus = false;
+void meshMsgHandler(const char *event, const char *data) {
+  Serial.printlnf("event=%s data=%s", event, data ? data : "NULL");
+  meshStatus = !meshStatus;
+  digitalWrite(D7, meshStatus);
+}
+
 void setup() {
+  Serial.begin(9600);
+
   pinMode(D7, OUTPUT);
+
+  // Setup some GW pinging stuff:
+#if PLATFORM_ID == 12
+  // Argon GW
+  //meshPinger.start();
+  selectExternalMeshAntenna();
+#define MESH_PING_ENABLE
+#elif PLATFORM_ID == 13
+  // Boron
+#elif PLATFORM_ID == 14
+  // Xenon
+  // Also, probably not GW so just listen for messages from the gateway...
+  Mesh.subscribe("mesh-msg", meshMsgHandler);
+#endif
 
   // Get matrix going!
   matrix.begin(0x70);
@@ -37,8 +77,13 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(D7, HIGH);
-  delay(1000);
-  digitalWrite(D7, LOW);
-  delay(1000);
+//  digitalWrite(D7, HIGH);
+//  delay(1000);
+//  digitalWrite(D7, LOW);
+//  delay(1000);
+
+#ifdef MESH_PING_ENABLE
+  Mesh.publish("mesh-msg", "GW ping");
+  delay(100);
+#endif
 }
